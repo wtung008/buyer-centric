696c7e7fd375af93ff31f2eb920b262b643ca971
wtung008 <wayne.h.tung@gmail.com>
2017-02-27 16:38:13 -0500

    Add new things to controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,22 +1,42 @@
 class ListingsController < ApplicationController
+  before_action! set_listing
   def index
+    @listings = Listing.all
   end
 
   def show
   end
 
   def new
+    @listing = Listing.new
   end
 
   def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def edit
+
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:list).permit(:title, :description, :price, :expiration, :category_id, :user_id)
   end
 end
