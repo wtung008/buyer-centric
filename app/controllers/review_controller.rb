c51c4c92c5d560daf42dcbc3d341ef68e3a0ce35
yonas samuel <yonastlolo@yahoo.com>
2017-02-28 13:57:38 -0500

    Add code to review.rb

--- app/controllers/review_controller.rb
+++ app/controllers/review_controller.rb
@@ -1,16 +1,52 @@
 class ReviewController < ApplicationController
+   before_action :set_listing
+   before_action :set_review, only [:show, :edit, :update, :destroy]
+
+
+  def new
+    @review = Review.new
+  end
+
+  def edit
+  end
+
+
+
+
   def create
     @review = Review.new(review_params)
+    @review.user_id = current_user.id
+    @review.listing_id = @listing_id
 
   if @review.save
-    redirect_to listings_url
+    redirect_to @listing
   else
-    render 'listings/new'
+    render @review.error
   end
 end
+
+  def update
+    if @review.update(review_params)
+      redirect_to @review
+    else
+      render @review.error
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to root_path
+  end
   private
+
+  def set_listing
+    @listing = Listing.find(params[:listing_id])
+  end
+
+  def set_review
+    @review = Review.find(params[:id])
+  end
   def review_params
-    params.require(:review).permit(:description, :listing_id, :user_id)
+    params.require(:review).permit(:rating, :comment)
 
   end
 end
