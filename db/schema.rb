ae4c87b39313f6ab5c8dd0540aaf1d3a72f394ac
wtung008 <wayne.h.tung@gmail.com>
2017-03-08 15:45:29 -0500

    Fix merge conflict.

--- db/schema.rb
+++ db/schema.rb
@@ -9,12 +9,8 @@
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
 # It's strongly recommended that you check this file into your version control system.
-#
-# <<<<<<< HEAD
-# ActiveRecord::Schema.define(version: 20170307211143) do
-# =======
+
 ActiveRecord::Schema.define(version: 20170308203139) do
->>>>>>> tokbox2
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,16 +99,13 @@ ActiveRecord::Schema.define(version: 20170308203139) do
     t.string   "avatar_content_type"
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
-# <<<<<<< HEAD
+    t.text     "address"
+    t.float    "latitude"
+    t.float    "longitude"
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-# =======
-    t.text     "address"
-    t.float    "latitude"
-    t.float    "longitude"
-# >>>>>>> tokbox2
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
   end
