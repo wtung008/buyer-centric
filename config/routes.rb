3f4c8ebdbfeddccf3fdf499562d49b6ffa8b9ee3
tasconajoey <joey.tascona@gmail.com>
2017-03-08 19:22:46 -0500

    Fix errors

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   resources :users, only: [:show] do
     resources :reviews, except: [:show, :index]
   end
-
+  resources :conversations do
+    resources :messages
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
